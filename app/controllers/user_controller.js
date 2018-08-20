440bc93a4def63d6c1741d8bc78685ce944bdccb
Dinesh kumar <Poladinesh2000@yahoo.com>
2018-08-20 15:57:57 -0500

    added draft methods to the user controller and added gender to the user model.

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,6 +1,48 @@
 const express = require('express')
 const app = express();
 const chalk = require('chalk');
+var User = require('./../models/userModel');
+var util = require('util');
+var moment = require('moment');
+
+// middleware that is specific to this router
+app.use(function timeLog(req, res, next) {
+  console.log('Time: ', moment().format('MMMM Do YYYY, h:mm:ss a'));
+  next();
+});
+
+app.route('/')
+  .get((req, res, next) => {
+    res.send('Information About the user.')
+  })
+
+  .post((req, res, next) => {
+    // create a new user called chris
+    // req.user = new User(req.body);
+    console.log(chalk.green("POST request at user post"));
+    // console.log(req.body);
+
+    next();
+  })
+
+  .post((req, res, next) => {
+    console.log("req.body: "+JSON.stringify(req.body));
+    // call the built-in save method to save to the database
+    // req.user.save(function(err) {
+    //   if (err) throw err;
+    //   res.send('User saved successfully!');
+    //   console.log('User saved successfully!');
+    // });
+res.send('User saved successfully!');
+  })
+
+  .put((req, res, next) => {
+    res.send('animals home page.PUT.app.route')
+  })
+
+  .delete((req, res, next) => {
+    res.send('animals home page.DELETE.app.route')
+  })
 
 
 
--- app/models/userModel.js
+++ app/models/userModel.js
@@ -8,6 +8,7 @@ var userSchema = new Schema({
   firstName: String,
   userName: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  gender: String,
   isAdmin: Boolean,
   userImageLocation: String,
   location: String,
@@ -25,3 +26,23 @@ var User = mongoose.model('User', userSchema);
 
 // make this available to our users in our Node applications
 module.exports = User;
+
+
+// ({
+//   userNumber: Number,
+//   firstName: String,
+//   userName: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   isAdmin: Boolean,
+//   gender: String,
+//   userImageLocation: String,
+//   location: String,
+//   meta: {
+//     mobile: Number,
+//     email: String,
+//     age: Number,
+//     website: String
+//   },
+//   created_at: Date,
+//   updated_at: Date
+// });
