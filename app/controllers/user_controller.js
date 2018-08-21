63b348b4aac5abde09816977f8aebc86538baba4
Dinesh kumar <Poladinesh42@gmail.com>
2018-08-21 15:50:17 -0500

    draft working copy for database insertion.

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -13,27 +13,63 @@ app.use(function timeLog(req, res, next) {
 
 app.route('/')
   .get((req, res, next) => {
-    res.send('Information About the user.')
+    User.find(function(err, Users) {
+      if (err) {
+        res.send(err);
+        return console.error(err);
+      }
+      console.log(Users);
+      res.json(Users);
+
+    });
   })
 
   .post((req, res, next) => {
     // create a new user called chris
     // req.user = new User(req.body);
     console.log(chalk.green("POST request at user post"));
-    // console.log(req.body);
+console.log(req.body);
+    // var newUser = new User({
+    //   userNumber: 1,
+    //   firstName: "Dinesh Kumar",
+    //   userName: "poladinesh423@gmail.com",
+    //   password: "dinesh",
+    //   isAdmin: true,
+    //   gender: "M",
+    //   userImageLocation: "google",
+    //   location: "drive",
+    //   meta: {
+    //     mobile: 4054016751,
+    //     email: "poladinesh42@gmail.com",
+    //     age: 27,
+    //     website: "not yet"
+    //   },
+    //   created_at: Date.now(),
+    //   updated_at: Date.now()
+    // });
+    // newUser.save(function(err, newUser) {
+    //   if (err) {
+    //     // return console.error(err);
+    //     res.json(err);
+    //   } else if (newUser){
+    //     res.send('User saved successfully!'+ newUser);
+    //   }
+    //
+    //   console.log(chalk.blue("SUCCESS: " + newUser));
+    //   next();
+    // });
 
-    next();
   })
 
   .post((req, res, next) => {
-    console.log("req.body: "+JSON.stringify(req.body));
+    console.log("req.body: " + JSON.stringify(req.body));
     // call the built-in save method to save to the database
     // req.user.save(function(err) {
     //   if (err) throw err;
     //   res.send('User saved successfully!');
     //   console.log('User saved successfully!');
     // });
-res.send('User saved successfully!');
+    res.send('from second post');
   })
 
   .put((req, res, next) => {
@@ -43,11 +79,6 @@ res.send('User saved successfully!');
   .delete((req, res, next) => {
     res.send('animals home page.DELETE.app.route')
   })
-
-
-
-
-
 
 
 
