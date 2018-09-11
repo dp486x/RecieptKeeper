ba17f085cfcd308259fd55549055b3f1154f6f8c
Dinesh kumar <Poladinesh42@gmail.com>
2018-09-11 16:37:12 -0500

    Image now upload to the day folder. If that is the first image, a new folder with the date will be created.

--- app/controllers/receipt_controller.js
+++ app/controllers/receipt_controller.js
@@ -1,14 +1,36 @@
 const express = require('express');
-var multer = require('multer'); // jshint ignore:line
-var upload = multer({ // jshint ignore:line
-    dest: 'uploads/'
-});
+const multer = require('multer');
+var bodyParser = require('body-parser');
+const crypto = require('crypto');
+var path = require('path');
+const mkdirp = require('mkdirp');
+const Receipt = require('./../models/receiptModel');
 const app = express();
 const chalk = require('chalk');
-var util = require('util'); // jshint ignore:line
-var moment = require('moment');
-const fs = require('fs'); // jshint ignore:line
-// app.use(express.bodyParser());
+const moment = require('moment');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        const dir = "./Receipts/" + moment().format('YYYY') + "/" +
+            moment().format("MMMM") + "/" + moment().format('DD') +
+            "(" + moment().format('dddd') + ")";
+
+        mkdirp(dir, err => cb(err, dir));
+    },
+    filename: function(req, file, cb) {
+        crypto.pseudoRandomBytes(16, function(err) { //(err,raw)
+            if (err) {
+                return cb(err);
+            }
+            cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+            // cb(null, raw.toString('hex') + path.extname(file.originalname));
+        });
+    }
+});
+var upload = multer({
+    storage: storage
+});
+
+app.use(bodyParser.json());
 
 // middleware that is specific to this router
 app.use(function timeLog(req, res, next) {
@@ -23,24 +45,64 @@ app.route('/')
         next();
     })
 
+    // .post(upload.any(), (req, res, next) => {111
+    .post(upload.any(), (req, res) => {
+        console.log("req.body: POST");
+
+        debugger; // jshint ignore:line
+
+        // upload(req, res, function(err) {
+        //     if (err) {
+        //         res.send("Something went wrong!");
+        //     }
+        //     res.send("File uploaded sucessfully!.");
+        // });
+        res.send('receipt saved successfully!');
+        // next();
+    })
+
     .post((req, res, next) => {
         // create a new user called chris
         // req.user = new User(req.body);
         console.log(chalk.green("POST request at receipt post"));
-        // console.log(req.body);
-
-        next();
-    })
-
-    .post((req, res, next) => {
-        console.log("req.body:" + req.body);
-        // call the built-in save method to save to the database
-        // req.user.save(function(err) {
-        //   if (err) throw err;
-        //   res.send('User saved successfully!');
-        //   console.log('User saved successfully!');
-        // });
-        res.send('receipt saved successfully!');
+        var newReceipt = new Receipt({
+            receiptNumber: 1, // receipt # until date
+            receiptName: "String", //receiptFrom+receiptMonth-receiptDate-receiptYear
+            businessName: "String", // company name
+            businessId: 12, //company ID in the DB
+            receiptFullDate: Date.now(), //Date.now()
+            receiptYear: moment()
+                .format('YYYY'), //moment().format('YYYY')
+            receiptMonth: moment()
+                .format("MMM"), //moment().format("MMM")
+            receiptDate: moment()
+                .format('dd'), //moment().format('dd');
+            receiptDay: moment()
+                .format('dddd'), //moment().format('dddd');
+            subTotal: 2, // amount before tax
+            tax: 2, // total tax
+            total: 2, //amount with Tax
+            receiptLocation: "String", //receipts/receiptYear/receiptMonth/receiptDate/receiptName.format
+            category: "String", // category to which receipt belongs to(grocery, gas, clothing)
+            savedBy: "String", // User who saved the receipt
+            modifiedBy: "String", //User modified the receipt
+            created_at: Date.now(), // receipt created on
+            updated_at: Date.now() // receipt updated on
+            // discounts:{
+            //   actualPrice: Number;
+            //   discountPrice: Number;
+            //   discountPercent: Number;
+            //
+            // }
+        });
+        newReceipt.save(function(err, newUser) {
+            if (err) {
+                // return console.error(err);
+                res.json(err);
+            } else if (newUser) {
+                res.send('User saved successfully!' + newUser);
+            }
+        });
         next();
     })
 
